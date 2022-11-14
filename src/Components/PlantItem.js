8f5e24d9543a4825a361796750245c5a8f51701d
mariam <mariameseydou99@gmail.com>
2022-11-14 12:29:15 +0000

    first commit

--- src/Components/PlantItem.js
+++ src/Components/PlantItem.js
@@ -2,10 +2,12 @@ import React from 'react'
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-
+function handleClick(plantName){
+    alert(`Vous avez achete 1 ${plantName}? Tres bon choix✨`)
+}
 function PlantItem({id,name,cover,water,light}) {
   return (
-    <li key={id} className='lmj-plant-item'>
+    <li key={id} className='lmj-plant-item' onClick={()=> handleClick(name)}>
         <img src={cover} alt={`${name} cover`} className='lmj-plant-item-cover'/>
         {name}
         <div>
